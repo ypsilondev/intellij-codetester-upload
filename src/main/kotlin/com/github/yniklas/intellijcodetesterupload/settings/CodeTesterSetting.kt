d76e8266f277d4e6421a656e857891431e3d3c31
Gregyyy <gregyyy@pm.me>
2021-12-07 22:09:00 +0100

    Add option to save all files before testing

--- src/main/kotlin/com/github/yniklas/intellijcodetesterupload/settings/CodeTesterSetting.kt
+++ src/main/kotlin/com/github/yniklas/intellijcodetesterupload/settings/CodeTesterSetting.kt
@@ -14,6 +14,7 @@ import com.intellij.util.xmlb.XmlSerializerUtil
 class CodeTesterSetting : PersistentStateComponent<CodeTesterSetting> {
 
     var uniProject = false
+    var saveBeforeTesting = true
 
     companion object {
         fun getInstance(project: Project): CodeTesterSetting {
